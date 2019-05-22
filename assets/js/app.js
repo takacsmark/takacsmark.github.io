e3a6b6bc46f22bf0fc7bf51f0eb77f12ba9a543e
takacsmark <takacs.mark@gmail.com>
2019-05-22 10:54:22 +0200

    double image layout

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,3 @@
-$(document).foundation();
-
 $(function() {
 
     //Display multiple images as gallery in posts
@@ -7,47 +5,9 @@ $(function() {
 
         var nodes = $(this).parent().children();
 
-        nodes.wrapAll('<div class="row gallery"></div>');
-        nodes.addClass("small-6 columns");
+        nodes.addClass("w-50 p-1 float-left");
     });
 
-    //Make embedded videos fill the widht of the post
-    $("article .video-thumb").addClass("flex-video");
-
-    //Main page slideshow
-    const slides = ['docker-book', 'dockerfile-tutorial'];
-    let activeSlide = 0;
-
-    function flipSlide() {
-
-        let newSlide = activeSlide + 1;
-        if(newSlide > slides.length -1 ) {
-            newSlide = 0;
-        }
-
-        let activeElement = $("#" + slides[activeSlide]);
-        let newElement = $("#" + slides[newSlide]);
-
-        activeElement.addClass('slideout');
-
-        activeElement.one("transitionend", function(event) {
-            activeElement.addClass('offscreen');
-            activeElement.removeClass('slideout');
-
-            newElement.removeClass('offscreen');
-            newElement.addClass('slidein');
-
-            newElement.one("transitionend", function(event) {
-                newElement.removeClass('slidein');
-
-                activeSlide = newSlide;
-                setTimeout(flipSlide, 6000);
-            });
-        });
-    }
-
-    setTimeout(flipSlide, 6000);
-    
     //Hide and show share buttons
     let sheight = $(".social-float .social-share").height();
     
