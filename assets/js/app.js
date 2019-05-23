10bc579e21ba33fd0cd31b384741ee8632c2053f
takacsmark <takacs.mark@gmail.com>
2019-05-23 18:00:34 +0200

    sticky sidebar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,15 @@
 $(function() {
+
     //Display multiple images as gallery in posts
     $("article img ~ img").each(function(){
         var nodes = $(this).parent().children();
         nodes.addClass("w-50 p-1 float-left");
     });
+
+    //Sticky sidebar
+    $("#sidebar-book-promo").stick_in_parent({
+        parent: ".sticky-container",
+	offset_top: 75 
+    });
+    
 });
