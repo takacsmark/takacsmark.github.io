815caa81e6eedc3cd6093deee9ef4ceaa91c997c
takacsmark <takacs.mark@gmail.com>
2020-04-26 11:45:53 +0200

    viewcounter

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 $(function() {
 
-    //Display multiple images as gallery in posts
+    //Displays multiple images as gallery in posts
     $("article img ~ img").each(function(){
         var nodes = $(this).parent().children();
         nodes.addClass("w-50 p-1 float-left");
@@ -40,4 +40,58 @@ $(function() {
 	}
 
     }
+
+    /***************************************
+     * Page view count
+     **************************************/
+    var db = firebase.firestore();
+
+    if(window.location.pathname != "/") {
+        //Displays view count on post pages
+        let urlPath = window.location.pathname.replace("/", "");
+        let docRef = db.collection("page-stats").doc(urlPath);
+        
+        docRef.onSnapshot(function(doc) {
+            if(!doc.metadata.hasPendingWrites) {
+                console.log("Server data: ", doc.data());
+                document.getElementById("view-count").innerHTML = " - " + doc.data().viewCount.toLocaleString() + " views";
+            }
+        });
+
+        docRef.set({
+            viewCount: firebase.firestore.FieldValue.increment(1)
+        }, {merge: true});
+
+    } else {
+        //Displays view counts on home page
+        let pageStatsRef = db.collection("page-stats");
+
+        pageStatsRef.onSnapshot(function(querySnapshot) {
+            querySnapshot.forEach(function(doc) {
+                node = document.getElementById("/" + doc.id + "/")
+                if(!!node){
+                    newHTML = " - " + doc.data().viewCount.toLocaleString() + " views"
+                    if(node.innerHTML != newHTML){
+                        node.innerHTML = newHTML;
+                    }
+                }
+            });
+        });
+
+        pageStatsRef
+        .get()
+        .then(function(querySnapshot) {
+            querySnapshot.forEach(function(doc) {
+                node = document.getElementById("/" + doc.id + "/")
+                if(!!node){
+                    node.innerHTML = " - " + doc.data().viewCount.toLocaleString() + " views";
+                }
+            });
+        })
+        .catch(function(error) {
+            console.log("Error getting documents: ", error);
+        });
+    }
+
+
 });
