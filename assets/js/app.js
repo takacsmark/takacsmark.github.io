81edaf1477882e1bb1a9330f0bb6268152b508a1
takacsmark <takacs.mark@gmail.com>
2020-04-28 18:41:28 +0200

    sidebar

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,12 +6,6 @@ $(function() {
         nodes.addClass("w-50 p-1 float-left");
     });
 
-    //Sticky sidebar
-    $("#sidebar-book-promo").stick_in_parent({
-        parent: ".sticky-container",
-	offset_top: 75 
-    });
-
     //Inline sponsored links
     if($("article").length){
     
