7e379e7fcf2d589a756281ec94c6525b6d602a6e
takacsmark <takacs.mark@gmail.com>
2021-02-22 19:13:46 +0100

    index page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const colors = require("./node_modules/tailwindcss/colors");
+
 module.exports = {
   purge: ["./src/**/*.html", "./src/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lime: colors.lime,
+        indigo: colors.lime,
+      },
+    },
   },
   variants: {
     extend: {},
